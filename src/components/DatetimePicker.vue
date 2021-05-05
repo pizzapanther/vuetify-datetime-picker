3c4c4146ef7fc83d8297485d9a1fc44ef8b9cd8a
Paul Bailey <paul.m.bailey@gmail.com>
2021-05-05 17:51:58 -0500

    dialogProps

--- src/components/DatetimePicker.vue
+++ src/components/DatetimePicker.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog v-model="display" :width="dialogWidth">
+  <v-dialog v-model="display" :width="dialogWidth" v-bind="dialogProps">
     <template v-slot:activator="{ on }">
       <v-text-field
         v-bind="textFieldProps"
@@ -115,6 +115,9 @@ export default {
       type: Object
     },
     timePickerProps: {
+      type: Object
+    },
+    dialogProps: {
       type: Object
     }
   },
